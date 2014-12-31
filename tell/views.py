44f1cf241b2dd979effa5e71911e4c81d9ea772e
Anton Blomberg <anton@deadlock.se>
2014-12-31 08:23:06 -0800

    catalog.json is now loaded from the remote host

--- tell/views.py
+++ tell/views.py
@@ -1,25 +1,24 @@
 from datetime import datetime
-import os
-from django.conf import settings
+from urllib2 import urlopen
 from django.shortcuts import render
 import json
 
-base_url = "http://localhost/"
+CATALOG_URL = "http://localhost:8001/catalog.json"
 
 CATALOG = None
-FILES_URL = "".join((settings.STATIC_URL, "files/"))
+FILES_URL = CATALOG_URL[:CATALOG_URL.rindex("/") + 1]
 
 def getCatalog():
     global CATALOG
     if CATALOG is None:
-        with open("files/catalog.json", "r") as f:
-            CATALOG = json.load(f)
-            for album in CATALOG.values():
-                album['date'] = datetime.fromtimestamp(album['date'])
-                for photo in album['photos']:
-                    photo['date'] = datetime.fromtimestamp(photo['date'])
+        response = urlopen(CATALOG_URL)
+        CATALOG = json.load(response)
+        for album in CATALOG.values():
+            album['date'] = datetime.fromtimestamp(album['date'])
+            for photo in album['photos']:
+                photo['date'] = datetime.fromtimestamp(photo['date'])
 
-                album['photos'].sort(key=lambda x: x['date'])
+            album['photos'].sort(key=lambda x: x['date'])
     return CATALOG
 
 
